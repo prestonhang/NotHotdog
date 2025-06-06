684068a678330d2a184bb549110cd340f38d8e43
prestonhang <prestontanhang@gmail.com>
2025-06-05 20:53:16 -0700

    add files

---
+++ NotHotdog/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NotHotdog
+//
+//  Created by Preston Hang on 6/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ NotHotdogTests/NotHotdogTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotHotdogTests.swift
+//  NotHotdogTests
+//
+//  Created by Preston Hang on 6/5/25.
+//
+
+import Testing
+@testable import NotHotdog
+
+struct NotHotdogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
