a0ebaa3e72082edd379ba7202fcd7fcbb36c337c
Preston <prestontanhang@gmail.com>
2025-12-08 00:29:39 -0800

    feat: adding data objects for running Image Analysis command for Apple Intelligence

---
+++ NotHotdog/Models/AITask.swift
@@ -0,0 +1,13 @@
+//
+//  AITask.swift
+//  NotHotdog
+//
+//  Created by Preston Hang on 12/7/25.
+//
+
+@Generable
+struct AITask {
+    let title: String
+    let description: String
+    
+}
--- NotHotdog/Views/AnalyzePhotoView.swift
+++ NotHotdog/Views/AnalyzePhotoView.swift
@@ -12,6 +12,7 @@ struct AnalyzePhotoView: View {
     @State private var selectedItem: PhotosPickerItem?
     @State private var selectedPhoto: UIImage?
     @State private var showCamera: Bool = false
+    @State private var isHotdog: Bool = false
     
     var body: some View {
         if let selectedPhoto = selectedPhoto {
@@ -20,6 +21,15 @@ struct AnalyzePhotoView: View {
                 .scaledToFit()
                 .frame(width: 300, height: 300)
                 .cornerRadius(20)
+            Button(action: { isHotdog = AnalyzeImageCommand(UIImage: )(selectedPhoto: selectedPhoto)}) {
+                if isHotdog {
+                    Text("It's a Hotdog!")
+                } else {
+                    Text("It's not a Hotdog...")
+                }
+            }
+        } else {
+            Text("No photo selected")
         }
         
         Button(action: { showCamera = true}) {
@@ -33,6 +43,28 @@ struct AnalyzePhotoView: View {
         }
         .sheet(isPresented: $showCamera) {
             CameraView(image: $selectedPhoto)
+        }
+        
+        PhotosPicker(selection: $selectedItem, matching: .images, photoLibrary: .shared()) {
+            Text("Select a photo")
+                .font(.headline)
+                .padding(20)
+                .frame(maxWidth: 300)
+                .background(Color.indigo)
+                .foregroundColor(.white)
+                .cornerRadius(50)
+        }
+        .onChange(of: selectedItem) { _, item in
+            if let item = item {
+                Task {
+                    if let data = try? await item.loadTransferable(type: Data.self) {
+                        selectedPhoto = UIImage(data: data)
+                        
+                    } else {
+                        print( "Failed to load data.")
+                    }
+                }
+            }
         }
     }
 }
